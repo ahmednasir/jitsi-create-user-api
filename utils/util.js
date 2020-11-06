2d123468c08766de7f1eb5b0d4fa596ae1c8cec0
Ubuntu <ubuntu@ip-172-31-16-39.ap-south-1.compute.internal>
2020-11-06 04:54:16 +0000

    updated queries

--- utils/util.js
+++ utils/util.js
@@ -1,7 +1,7 @@
 const exec = require('child_process').exec;
 module.exports.getEndTime = (minutes) => {
     let currentTime = new Date()
-    return new Date(currentTime.getTime() + minutes * 60000).toISOString()
+	return new Date(currentTime.getTime() + minutes * 60000).toISOString()
 }
 
 module.exports.execShellCommand = (cmd) => { 
